adf999e1b99568f0e618c21548d7ab74901cee8b
unknown <long.bvntp@gmail.com>
2021-05-23 09:40:37 +0700

    Commit

---
+++ EP1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secrect()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            //tạo userClaim
+            var grandmaClaim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Long"),
+                new Claim(ClaimTypes.Email, "long@gmail.com"),
+                new Claim("Grandma says", "long@gmail.com")
+            };
+            var licenseClaim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Long nc"),
+                new Claim("Grandma says", "long@gmail.com")
+            };
+
+            //tạo user Identidy
+            var grandIdentity = new ClaimsIdentity(grandmaClaim, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaim, "Pikapu");
+
+            var userPrincipal = new ClaimsPrincipal(new[] {grandIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
